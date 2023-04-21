7613f239edd97ffdab16a7e9ca8c9add9c84758d
lufanl09 <lufanl@uri.edu>
2023-04-21 15:21:46 -0400

    Update challenge7.py

--- Coding Challenge 7/challenge7.py
+++ Coding Challenge 7/challenge7.py
@@ -2,6 +2,24 @@
 # Challenge 7
 ###############
 
+# The goal of Coding Challenge 7 is to convert the Coding Challenge 5 exercise to work with temporary folders,
+# os.path.join and glob.glob.
+
+# Requirements from Coding Challenge 5 as reference:
+# 1. The two input species data must be in a SINGLE CSV file, you must process the input data to separate out the species.
+#    I recommend downloading the species data from the same source so the columns match.
+# 2. Only a single line of code needs to be altered (workspace environment) to ensure code runs on my computer, and you
+#    provide the species data along with your Python code.
+# 3. The heatmaps are set to the right size and extent for your species input data, i.e. appropriate fishnet cellSize.
+# 4. You leave no trace of execution, except the resulting heatmap files.
+# 5. You provide print statements that explain what the code is doing, e.g. Fishnet file generated.
+
+# The species for this coding challenge are Chrysaora colorata and Haliscera bigelowi. The data is downloaded from OBIS.
+
+# Preliminary step: please change the workspace to your working directory on line 17 before running the code.
+
+
+# Importing tools that will be used for this challenge
 import csv
 import os
 import arcpy
@@ -10,6 +28,7 @@ input_directory = r"C:\NRS528\Class_07\challenge7"
 csv_file = r"species.csv"
 
 ### Step 1. Read csv and extract the 2 species names into a list
+# create a species list to store the two species
 species_list = []
 
 with open(os.path.join(input_directory, csv_file)) as species_file:
@@ -47,15 +66,15 @@ for species in species_list:
     file.close()
 
     new_directory = r"Species_Directory"
-    arcpy.env.workspace = new_directory
+    arcpy.env.workspace = os.path.join(input_directory, new_directory)
 
     # Inputs for XY Event Layer
-    in_Table = r"Species_Directory/" + str(species) + ".csv"
+    in_Table = str(species) + ".csv"
     x_coords = "Longitude"
     y_coords = "Latitude"
     z_coords = ""
     out_Layer = str(species)
-    saved_Layer = r"Species_Directory/" + str(species) + "_Output.shp"
+    saved_Layer = str(species) + "_Output.shp"
 
     # Spatial reference for the coordinates
     spRef = arcpy.SpatialReference(4326)  # 4326 == WGS 1984
@@ -69,7 +88,7 @@ for species in species_list:
         print("Created file successfully " + str(species))
 
     ### Step 4. Describe shp to get extent
-    desc = arcpy.Describe(r"Species_Directory/" + str(species) + "_Output.shp")
+    desc = arcpy.Describe(str(species) + "_Output.shp")
 
     print('Extent:\n XMin: {},\n YMin: {}'.format(desc.extent.XMin, desc.extent.YMin))
     print('Extent:\n XMax: {},\n YMax: {}'.format(desc.extent.XMax, desc.extent.YMax))
