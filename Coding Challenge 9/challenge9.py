b5b4f3b14220cb28a2b8bde47f52f9313c09382c
lufanl09 <lufanl@uri.edu>
2023-04-21 15:39:22 -0400

    Uploading cc9

--- Coding Challenge 9/challenge9.py
+++ Coding Challenge 9/challenge9.py
@@ -1,13 +1,30 @@
 ###############
-# Challenge 9 #
+# Challenge 9
 ###############
 
+# Prompt:
+# In this coding challenge, your objective is to utilize the arcpy.da module to undertake some basic partitioning of
+# your dataset. I want you to work with the Forest Health Works dataset from RI GIS
+
+# Using the arcpy.da module, I want you to extract all sites that have a photo of the invasive species into
+# a new Shapefile, and do some basic counts of the dataset.
+
+# In summary, please addressing the following:
+# 1. Count how many individual records have photos, and how many do not (2 numbers), print the results.
+# 2. Count how many unique species there are in the dataset, print the result.
+# 3. Generate two shapefiles, one with photos and the other without.
+
+# Preliminary step: please change your arcpy workspace on line 22 before running the code.
+
+# Importing tool
 import arcpy
 arcpy.env.overwriteOutput = True
 
+# Setting workspace
 arcpy.env.workspace = r"C:\NRS528\Class_09\challenge9"
-input_shp = r"C:\NRS528\Class_09\challenge9\RI_Forest_Health_Works_Project%3A_Points_All_Invasives.shp"
+input_shp = r"RI_Forest_Health_Works_Project%3A_Points_All_Invasives.shp"
 
+# Creating file names for shapefile with photo and shapefile without photo
 new_shpfile_photo = arcpy.CreateFeatureclass_management(arcpy.env.workspace, "new_shapefile_photo.shp", "POINT", input_shp,
                                     "DISABLED", "DISABLED", spatial_reference=4326)
 
@@ -15,8 +32,8 @@ new_shpfile_nophoto = arcpy.CreateFeatureclass_management(arcpy.env.workspace, "
                                     "DISABLED", "DISABLED", spatial_reference=4326)
 
 
-### Count how many individual records have photos, and how many do not (2 numbers), print the results.
-# with photos
+### Step 1. Count how many individual records have photos, and how many do not (2 numbers), print the results.
+# Records with photos in the shapefile
 count = 0
 with arcpy.da.SearchCursor(input_shp, ['photo']) as cursor:
     for row in cursor:
@@ -24,7 +41,7 @@ with arcpy.da.SearchCursor(input_shp, ['photo']) as cursor:
             count += 1
 print("There are " + str(count) + " records with photos.")
 
-# without photos
+# Records without photos in the shapefile
 count = 0
 with arcpy.da.SearchCursor(input_shp, ['photo']) as cursor:
     for row in cursor:
@@ -33,7 +50,8 @@ with arcpy.da.SearchCursor(input_shp, ['photo']) as cursor:
 print("There are " + str(count) + " records without photos.")
 
 
-### Count how many unique species there are in the dataset, print the result.
+### Step 2. Count how many unique species there are in the dataset, print the result.
+# Create a list to store the species
 species_list = []
 
 line_count = 0
@@ -45,11 +63,11 @@ with arcpy.da.SearchCursor(input_shp, ['Species']) as cursor:
 print("There are " + str(len(species_list)) + " unique species in the dataset.")
 
 
-### Generate two shapefiles, one with photos and the other without.
+### Step 3. Generate two shapefiles, one with photos and the other without.
 # list fields tool
 field_list = [f.name for f in arcpy.ListFields(input_shp)]  # generates all fields
 
-# with photo
+# Generate a shapefile of fields with photos
 expression = arcpy.AddFieldDelimiters(input_shp, "photo") + " = 'y'"
 with arcpy.da.SearchCursor(input_shp, field_list, expression) as sCursor:
     with arcpy.da.InsertCursor(new_shpfile_photo, field_list) as iCursor:
@@ -57,7 +75,7 @@ with arcpy.da.SearchCursor(input_shp, field_list, expression) as sCursor:
             iCursor.insertRow(row)
 print("New shapefile with photos is created.")
 
-# without photo:
+# Generate a shapefile of fields without photos
 expression = arcpy.AddFieldDelimiters(input_shp, "photo") + " <> 'y'"
 with arcpy.da.SearchCursor(input_shp, field_list, expression) as sCursor:
     with arcpy.da.InsertCursor(new_shpfile_nophoto, field_list) as iCursor:
@@ -65,5 +83,5 @@ with arcpy.da.SearchCursor(input_shp, field_list, expression) as sCursor:
             iCursor.insertRow(row)
 print("New shapefile with no photos is created.")
 
-
+print("End of assignment")
 
